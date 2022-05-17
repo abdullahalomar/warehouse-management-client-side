6aab5a723adf26a91f2731b413b31605d92bf7cc
unknown <abdullahalomar048@gmail.com>
2022-05-17 23:21:32 +0600

    login data code added

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const emailRef = useRef();
+  const passRef = useRef();
+  const navigate = useNavigate();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const pass = passRef.current.value;
+    console.log(email, pass);
+  }
+
+  const navigateRegister = event => {
+    navigate('/register');
+  }
     return (
         <div className='container mt-4'>
             <h2 className='mb-3'>PLease Login</h2>
       
-            <Form>
-  <Form.Group className="mb-3" controlId="formBasicText">
+  <Form onSubmit={handleSubmit}>
+  {/* <Form.Group className="mb-3" controlId="formBasicText">
     <Form.Label>Name</Form.Label>
     <Form.Control type="text" placeholder="Enter Your Name" required/>
-  </Form.Group>
+  </Form.Group> */}
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" required/>
+    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required/>
     <Form.Text className="text-muted">
       We'll never share your email with anyone else.
     </Form.Text>
@@ -21,7 +36,7 @@ const Login = () => {
 
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" required/>
+    <Form.Control ref={passRef} type="password" placeholder="Password" required/>
   </Form.Group>
   <Form.Group className="mb-3" controlId="formBasicCheckbox">
     <Form.Check type="checkbox" label="Check me out" />
@@ -29,7 +44,10 @@ const Login = () => {
   <Button variant="secondary" type="submit">
     Submit
   </Button>
-</Form>
+        </Form>
+        <div className='mt-3'>
+        <h6>Are you new here? <Link to='/register' className='text-secondary pe-auto text-decoration-none' onClick={navigateRegister}>Please Register</Link></h6>
+        </div>
   
         </div>
     );
