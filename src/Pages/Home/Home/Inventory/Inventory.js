f3db508c27e587a1151c639f147728c875f2b3e1
unknown <abdullahalomar048@gmail.com>
2022-05-23 16:00:41 +0600

    card design added

--- src/Pages/Home/Home/Inventory/Inventory.js
+++ src/Pages/Home/Home/Inventory/Inventory.js
@@ -9,22 +9,21 @@ const Inventory = (props) => {
     }
     return (
         
-        <div className='col-sm-12 col-md-6 col-lg-4 gx-5 gy-3 mb-5'>
-            <div class='card' style={{width: '18rem'}}>
-            <img src={picture} class="card-img-top" width={100} alt=""  />
-            <div class='card-body'>
-                <h5 class='card-title'>{name}</h5>
-                <h5>Price: ${price}</h5>
-                <h6>Quantity: {quantity}</h6>
-                <h6>Supplier: {supplier_name}</h6>
-                    <p class='card-text'>{short_desc}</p>
+        <div className='col-sm-12 col-md-6 col-lg-4 gx-5 gy-3 mb-5'> 
+            <div class="card h-100">
+                <img src={picture} class="card-img-top"  alt="..."  />
+                <div class="card-body">
+                    <h5 class="card-title">{name}</h5>
+                    <h6>Price: ${price}</h6>
+                    <h6>Quantity: {quantity}</h6>
+                    <h6>Supplier: {supplier_name}</h6>
+                    <p class="card-text">{short_desc}</p>
+                </div>
+                <div class="card-footer">
                     <button onClick={()=> NavigateInventoryDetails(_id)} className='btn btn-secondary'>Stock Update</button>
-            </div>
-            </div>
+                </div>
+            </div>         
         </div>
-                  
-      
-       
     );
 };
 
